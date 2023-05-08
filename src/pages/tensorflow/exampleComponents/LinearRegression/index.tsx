32f1fa117a4c8077f7588a2ab1c33a198d0183bb
webNjh <nijiahui-329@163.com>
2023-05-08 22:48:51 +0800

    线性回归例子

--- src/pages/tensorflow/exampleComponents/LinearRegression/index.tsx
+++ src/pages/tensorflow/exampleComponents/LinearRegression/index.tsx
@@ -1,26 +1,30 @@
 /*
 * 线性回归
 */
-import React, { useEffect, FC } from 'react';
+import { useEffect, FC } from 'react';
 import * as tfvis from '@tensorflow/tfjs-vis';
 import * as tf from '@tensorflow/tfjs';
+
+type TensorData = Array<number>;
 
 const LinearRegression: FC = () => {
 
   // 可视化散点图
-  const showScatterPlot = () => {
-    const xs = [1, 2, 3, 4];
-    const xy = [1, 3, 5, 7];
+  const showScatterPlot = (xs: TensorData, ys: TensorData) => {
 
     tfvis.render.scatterplot(
       { name: '线性回归训练集' },
-      { values: xs.map((x, i) => ({ x, y: xy[i] })) },
+      { values: xs.map((x, i) => ({ x, y: ys[i] })) },
       { xAxisDomain: [0, 5], yAxisDomain: [0, 8] }
     )
 
   }
 
-  const initTensorModel = () => {
+  const initTensorModel = async () => {
+    const xs: TensorData = [1, 2, 3, 4];
+    const ys: TensorData = [1, 3, 5, 7];
+
+    showScatterPlot(xs, ys);
     // 创建一个连续的模型
     const model = tf.sequential();
     // 定义一个全连阶层
@@ -33,7 +37,18 @@ const LinearRegression: FC = () => {
     // 设置损失函数：均方误差（MSE）
     model.compile({ 
       loss: tf.losses.meanSquaredError, // 损失算法
-      optimizer: tf.train.sgd(0.1) // 优化器
+      optimizer: tf.train.sgd(0.1) // 优化器(学习率)
+    });
+
+    const inputs = tf.tensor(xs);
+    const labels = tf.tensor(ys);
+    await model.fit(inputs, labels, {
+      batchSize: 4, // 一次训练选取多样本数量
+      epochs: 100, // 迭代次数
+      callbacks: tfvis.show.fitCallbacks(
+        { name: '训练过程' },
+        ['loss'],
+      )
     });
   }
 
@@ -41,6 +56,7 @@ const LinearRegression: FC = () => {
 
 
   useEffect(() => {
+    initTensorModel();
   }, [])
 
   return (
