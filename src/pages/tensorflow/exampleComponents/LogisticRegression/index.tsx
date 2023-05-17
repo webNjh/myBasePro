b09857a35c63e609272bc4acc7f9fe89cfed0126
n1919 <nijh@dtdream.com>
2023-05-17 20:19:49 +0800

    逻辑回归

--- src/pages/tensorflow/exampleComponents/LogisticRegression/index.tsx
+++ src/pages/tensorflow/exampleComponents/LogisticRegression/index.tsx
@@ -1,24 +1,33 @@
 /*
 * 逻辑回归
 */
+import { useEffect, useState } from 'react';
 import * as tfvis from '@tensorflow/tfjs-vis';
 import * as tf from '@tensorflow/tfjs';
+import { TensorData } from '../common';
 import { getData } from './utils';
 
 function LogisticRegression() {
   // 可视化散点图
   const showScatterPlot = (xs: TensorData, ys: TensorData) => {
-
+    const data = getData(400);
     tfvis.render.scatterplot(
       { name: '逻辑回归训练数据' },
       { values: [
-          ...xs.map((x, i) => ({ x, y: ys[i], label: ys[i] === 1 ? 'red' : 'blue' })),
+          data.filter(p => p.label === 1),
+          data.filter(p => p.label === 0),
         ] 
       },
-      { xAxisDomain: [140, 180], yAxisDomain: [30, 70] }
     )
 
+    const model = tf.sequential();
+    model.add(tf.layers.dense({
   }
+
+  useEffect(() => {
+    // showScatterPlot();
+  }, [])
+
   return (
     <div>
       LogisticRegression
